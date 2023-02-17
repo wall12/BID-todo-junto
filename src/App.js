5c426b85334da8e41f910ea491ee627a64dc0360
wall12 <espinola.walter512@gmail.com>
2023-02-17 01:59:27 -0300

    Ejercicio Core Todo-junto

--- src/App.js
+++ src/App.js
@@ -6,27 +6,15 @@ function App() {
   return (
     <div className="App">
       <PersonCard
-      firstname="Doe"
-      lastname="Jane"
-      age="45"
+      firstname="Jane"
+      lastname="Doe"
+      age={"45"}
       haircolor="black"
       />
       <PersonCard
       firstname="John"
       lastname="Smith"
-      age="88"
-      haircolor="Brown"
-      />
-      <PersonCard
-      firstname="Millard"
-      lastname="Fillmore"
-      age="50"
-      haircolor="Brown"
-      />
-      <PersonCard
-      firstname="Maria"
-      lastname="Smith"
-      age="62"
+      age={"88"}
       haircolor="Brown"
       />
     </div>
--- src/componentes/PersonCard.jsx
+++ src/componentes/PersonCard.jsx
@@ -1,15 +1,22 @@
 import { Component } from "react";
 
 class PersonCard extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            count: parseInt(this.props.age)
+        };
+    }
     render() { 
 
-        const {firstname, lastname, age, haircolor} = this.props;
+        const {firstname, lastname, haircolor} = this.props;
 
         return (
             <>
             <h1>{lastname},{firstname}</h1>
-            <p>age: {age}</p>
+            <p>age: {this.state.count}</p>
             <p>haircolor: {haircolor}</p>
+            <button onClick={()=> this.setState({count: this.state.count+1})}>Birthday Button for { firstname } { lastname }</button>
             </>
         );
     }
